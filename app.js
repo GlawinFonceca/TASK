fa54a5a439ce390c100fde2f68cab25f1038ff4c
GlawinFonceca <glawin.f@juegostudioz.com>
2022-10-14 17:03:31 +0530

    task

---
+++ app.js
@@ -0,0 +1,208 @@
+const express = require('express')
+const fs = require('fs');
+const path = require('path');
+const hbs = require('hbs')
+const validate=require('validator')
+const port = 2000;
+const bodyParser = require('body-parser');
+const { validateHeaderValue } = require('http');
+const app = express()
+
+const path1 = path.join(__dirname, '../TASK/backend');
+const viewPath = path.join(__dirname, '../TASK/backend/views');
+const headerPath = path.join(__dirname, '../TASK/backend/views/partials');
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath);
+hbs.registerPartials(headerPath);
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path1))
+
+
+
+
+app.get('', (req, res) => {
+    res.render('home', {
+        title: 'Home Page'
+    })
+})
+
+
+
+app.get('/pageSignup', (req, res) => {
+    res.render('signup', {
+        title: 'Signup Page'
+    })
+})
+
+
+
+
+app.get('/pageLogin', (req, res) => {
+    res.render('login', {
+        title: 'Login Page'
+    })
+})
+
+
+
+app.get('/pageProfile', (req, res) => {
+    res.render('profile', {
+        title: 'Profile Page'
+    })
+})
+
+
+
+
+
+
+app.post('/userSignup', (req, res) => {
+    try {
+        const uname =validate.isAlpha(req.body.name);
+        const uemail =validate.isEmail(req.body.email);
+        const upassword = validate.isAlphanumeric(req.body.password);
+        const passwordlength = validate.isLength(req.body.password,6,undefined)
+        const uphone = validate.isMobilePhone(req.body.phone);
+        
+        if(uname === true && uemail === true && upassword ===true && passwordlength === true && uphone ===true){
+        const dataBuffer = fs.readFileSync('user.json');
+        const dataJson = dataBuffer.toString();
+        const Users = JSON.parse(dataJson);
+        const duplicate = Users.filter((note) => {
+            return note.email === req.body.email && note.phone === req.body.phone && note.password === req.body.password;
+        })
+        if (duplicate.length === 0) {
+            const body = req.body;
+            Users.push(body)
+            const data = JSON.stringify(Users);
+            fs.writeFileSync('user.json', data)
+            return res.render('login')
+        }
+        res.render('signup',{
+            message:'User already exists. please enter diffrent credentials'
+        });
+        
+    }else{
+        console.log('failed')
+    }
+
+    }
+    catch (e) {
+        res.send({
+            message: 'User already exists',
+            data: e
+        })
+
+    }
+})
+
+
+
+
+app.post('/userLogin', (req, res) => {
+    try {
+        const dataBuffer = fs.readFileSync('user.json');
+        const dataJson = dataBuffer.toString();
+        const Users = JSON.parse(dataJson);
+        // Users.forEach( myFunction)
+        // function myFunction(user){
+        //     if(req.body.email === user.email && req.body.password === user.password ){
+        //         res.send({
+        //         message :'success',
+        //         data : user
+        //        })
+        //        return
+        //     }
+        // }
+        for (let user of Users) {
+            if (req.body.email === user.email && req.body.password === user.password) {
+                res.render('home',{
+                    title :'Home Page',
+                    message : `Hello ${user.name}`
+                })
+                return
+            }
+        }
+        for (let user of Users) {
+            if (req.body.email !== user.email && req.body.password !== user.password) {
+                res.render('home',{
+                    title :'Home Page',
+                    message : `Please Login`
+                })
+                return
+            }
+            else if(req.body.email !== user.email){
+                res.render('login',{
+                    title :'Login Page',
+                    message : `Invalid Email`
+                })
+                return
+
+            }
+            else if(req.body.password !== user.password){
+                res.render('login',{
+                    title :'Login Page',
+                    message : `Invalid Password`
+                })
+                return
+
+            }
+        }
+
+    }
+    catch (e) {
+        console.log('failed');
+        res.status(404).send({
+            message: 'failed',
+            data: e.message
+        })
+
+    }
+
+
+})
+
+
+
+app.post('/userProfile', (req, res) => {
+    try {
+        const dataBuffer = fs.readFileSync('user.json');
+        const dataJson = dataBuffer.toString();
+        const Users = JSON.parse(dataJson);
+        for (let user of Users) {
+            if (req.body.email === user.email) {
+                res.send({
+                    message: 'success',
+                    data: user
+                })
+                return
+            }
+        }
+        res.send('User not Found')
+
+
+    }
+    catch (e) {
+        console.log('failed');
+        res.status(404).send({
+            message: 'failed',
+            data: e.message
+        })
+
+    }
+
+})
+
+
+
+
+app.listen(port, () => {
+    console.log('server is up on port ' + port);
+})
+
+
+
+
